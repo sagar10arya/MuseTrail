163b72ef999e8391f2b618974ee9e460fd368504
sagar10arya <aryasagar125@gmail.com>
2024-10-06 11:30:40 +0530

    Getting started with blogs website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>A Blog Website Using Appwrite</h1>
+    </>
+  )
+}
+
+export default App
