56ec207a51bf74afcbf4f98af5e01578997ec698
sagar10arya <aryasagar125@gmail.com>
2024-10-08 19:52:51 +0530

    added production grade react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from '../components/Logo.jsx'
+import LogoutBtn from "./Header/LogoutBtn";
 
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
